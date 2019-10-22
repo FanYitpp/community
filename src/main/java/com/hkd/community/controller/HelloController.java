992b999bfd3bb94eef893f3db0bbe739723b6475
FanYitpp <254143937@qq.com>
2019-10-22 19:25:58 +0800

    test

--- src/main/java/com/hkd/community/controller/HelloController.java
+++ src/main/java/com/hkd/community/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(name ="name") String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "hellosfdsf";
 
     }
 }
