984f54a9d397bb94ab1f9dfd3d36b10307e06916
FanYitpp <254143937@qq.com>
2019-11-01 00:09:16 +0800

    add sql ddl

--- src/main/java/com/hkd/community/controller/AuthorizeController.java
+++ src/main/java/com/hkd/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.hkd.community.controller;
 
 import com.hkd.community.dto.AccessTokenDTO;
 import com.hkd.community.dto.GitUser;
+import com.hkd.community.mapper.UserMapper;
+import com.hkd.community.model.User;
 import com.hkd.community.provider.GitProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GitProvider gitProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${git.client.id}")
     private String clientId;
@@ -26,7 +34,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest httpServletRequest){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -36,7 +45,19 @@ public class AuthorizeController {
 
         String accessToken = gitProvider.getAccessToken(accessTokenDTO);
         GitUser gitUser = gitProvider.getUser(accessToken);
-        System.out.println(gitUser.getName());
-        return "index";
+        if(gitUser!=null){
+            User user = new User();
+            user.setName(gitUser.getName());
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccountId(String.valueOf(gitUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写cookie和session
+            httpServletRequest.getSession().setAttribute("user",gitUser);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
     }
 }
