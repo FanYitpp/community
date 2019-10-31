2bf4fcb324f00f01b2e196c97057856569616691
FanYitpp <254143937@qq.com>
2019-11-01 00:34:31 +0800

    持久化登陆状态

--- src/main/java/com/hkd/community/controller/AuthorizeController.java
+++ src/main/java/com/hkd/community/controller/AuthorizeController.java
@@ -5,13 +5,15 @@ import com.hkd.community.dto.GitUser;
 import com.hkd.community.mapper.UserMapper;
 import com.hkd.community.model.User;
 import com.hkd.community.provider.GitProvider;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest httpServletRequest){
+                           HttpServletResponse  response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -48,13 +50,13 @@ public class AuthorizeController {
         if(gitUser!=null){
             User user = new User();
             user.setName(gitUser.getName());
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setAccountId(String.valueOf(gitUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            //登陆成功，写cookie和session
-            httpServletRequest.getSession().setAttribute("user",gitUser);
+            response.addCookie(new Cookie("token" ,token));
             return "redirect:/";
         }else{
             return "redirect:/";
